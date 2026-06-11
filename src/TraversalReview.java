0b0ebabd1718680f4319bb1fc24d821d82229fc8
Viktor Huchiev <274128020+viktorHuc@users.noreply.github.com>
2026-06-11 09:47:01 -0700

    added concatenate method

--- src/TraversalReview.java
+++ src/TraversalReview.java
@@ -255,6 +255,11 @@ public class TraversalReview {
      * @return a string with all the values of the tree concatenated in-order
      */
     public static String concatenate(TreeNode node) {
-        return "";
+        String ret = "";
+        if(node == null) return ret;
+        ret += concatenate(node.left);
+        ret += node.data;
+        ret += concatenate(node.right);
+        return ret;
     }
 }
